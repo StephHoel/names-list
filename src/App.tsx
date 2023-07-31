402c01ed38201ed6c500ebd5330a72fa06ca94d9
Steph Hoel <steph.hoel@gmail.com>
2023-07-31 11:32:39 -0300

    build: Criando página React Puro

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from 'react-router-dom'
+import Home from './Home'
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" index element={<Home />} />
+
+      <Route path="names-list/" element={<Home />} />
+    </Routes>
+  )
+}
